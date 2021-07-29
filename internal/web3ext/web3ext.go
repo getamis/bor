e9f4ff4eddb13a2f7d868cc9575d45d5e91dbda4
ian <ian@maicoin.com>
2021-07-29 15:25:20 +0800

    *: add GetBlockReceipt apis

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -484,6 +484,12 @@ web3._extend({
 			call: 'debug_purgeCheckpointWhitelist',
 			params: 0,
 		}),
+		new web3._extend.Method({
+			name: 'getBlockReceipts',
+			call: 'debug_getBlockReceipts',
+			params: 1,
+			inputFormatter: [null],
+		}),
 	],
 	properties: []
 });
