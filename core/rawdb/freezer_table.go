afc107b1c3a4a78566fb0524485df7965901acb8
ian <ian@maicoin.com>
2021-07-29 21:32:18 +0800

    *: fix lint

--- core/rawdb/freezer_table.go
+++ core/rawdb/freezer_table.go
@@ -1049,7 +1049,7 @@ func (t *freezerTable) dumpIndex(w io.Writer, start, stop int64) {
 // Bor related changes
 //
 
-// Fill adds empty data till given number (convenience method for backward compatibilty)
+// Fill adds empty data till given number (convenience method for backward compatibility)
 func (t *freezerTable) Fill(number uint64) error {
 	if t.items.Load() < number {
 		b := t.newBatch()
