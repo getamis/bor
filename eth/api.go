2abcab184a9e9c87cd603789e65dc8bf8fbd7ac3
ian <ian@maicoin.com>
2021-08-19 11:34:31 +0800

    eth: fix missing get borReceipt

--- eth/api.go
+++ eth/api.go
@@ -548,26 +548,36 @@ func (api *PrivateDebugAPI) getModifiedAccounts(startBlock, endBlock *types.Bloc
 
 // GetBlockReceipts returns all transaction receipts of the specified block.
 func (api *PrivateDebugAPI) GetBlockReceipts(ctx context.Context, blockHash common.Hash) ([]map[string]interface{}, error) {
-	if receipts := api.eth.blockchain.GetReceiptsByHash(blockHash); receipts != nil {
-		if block := api.eth.blockchain.GetBlockByHash(blockHash); block != nil {
-			txs := block.Transactions()
-			if len(txs) != len(receipts) {
-				return nil, fmt.Errorf("txs length doesn't equal to receipts' length")
-			}
-
-			txReceipts := make([]map[string]interface{}, 0, len(txs))
-			blockNumber := block.Header().Number
-			for idx, receipt := range receipts {
-				tx := txs[idx]
-				fields, err := ethapi.ToTransactionReceipt(ctx, api.eth.APIBackend, tx, receipt, blockHash, tx.Hash(), blockNumber.Uint64(), uint64(idx))
-				if err != nil {
-					return nil, err
-				}
-				txReceipts = append(txReceipts, fields)
-			}
-
-			return txReceipts, nil
-		}
+	block := api.eth.blockchain.GetBlockByHash(blockHash)
+	if block == nil {
+		return nil, errors.New("block not found")
 	}
-	return nil, errors.New("unknown receipts")
+	blockNumber := block.Header().Number
+	receipts := api.eth.blockchain.GetReceiptsByHash(blockHash)
+	if receipts == nil {
+		return nil, errors.New("receipts not found")
+	}
+	txs := block.Transactions()
+	if len(txs) != len(receipts) {
+		return nil, fmt.Errorf("txs length doesn't equal to receipts' length")
+	}
+	txReceipts := make([]map[string]interface{}, 0, len(txs))
+	for idx, receipt := range receipts {
+		tx := txs[idx]
+		fields, err := ethapi.ToTransactionReceipt(ctx, api.eth.APIBackend, tx, receipt, blockHash, tx.Hash(), blockNumber.Uint64(), uint64(idx))
+		if err != nil {
+			return nil, err
+		}
+		txReceipts = append(txReceipts, fields)
+	}
+	receipt := rawdb.ReadBorReceipt(api.eth.chainDb, blockHash, blockNumber.Uint64())
+	if receipt != nil {
+		tx, _, _, _ := rawdb.ReadBorTransaction(api.eth.chainDb, receipt.TxHash)
+		fields, err := ethapi.ToTransactionReceipt(ctx, api.eth.APIBackend, tx, receipt, blockHash, receipt.TxHash, blockNumber.Uint64(), uint64(receipt.TransactionIndex))
+		if err != nil {
+			return nil, err
+		}
+		txReceipts = append(txReceipts, fields)
+	}
+	return txReceipts, nil
 }
